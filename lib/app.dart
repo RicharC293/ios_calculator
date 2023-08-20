cebf70ed977cc990dd1fd17628c7ebe034931e41
Richar Cangui <richarsantiagocangui96@gmail.com>
2023-08-19 21:00:36 -0500

    Implements ios calculator multiplatform

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:calculator_ios/screens/calculator_screen.dart';
+import 'package:flutter/cupertino.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      title: 'Flutter Demo',
+      home: CalculatorScreen(),
+    );
+  }
+}
